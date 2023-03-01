7f5e467cdd3511d38ccae0db5a4db466954dffce
HeavenlyRain <>
2023-03-01 14:30:19 -0500

    feat: add root route & remove pycache

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import uvicorn
+from loguru import logger
 from create_app import app
 
 # Do more stuff here??
 
 if __name__ == '__main__':
-    print("Running??!?!?!")
-    
+    logger.info("Starting Server...")
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
